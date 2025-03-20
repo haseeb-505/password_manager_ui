63e7c8af74cb0bb2c43afe1f2688903b29f4066b
haseeb-505 <hatahir87626@gmail.com>
2025-03-21 03:44:16 +0500

    publishing on github pages

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  
   plugins: [
     tailwindcss(),
     react()
   ],
+  base: "https://github.com/haseeb-505/password_manager_ui",
   
 })
